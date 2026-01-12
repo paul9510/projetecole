47a519002954f199828420c32a117dbe3f9fcf24
paul <paul@MacBook-Air-de-paul.local>
2026-01-12 17:24:37 +0100

    extends jpa

--- src/main/java/com/example/projet_ecole/repositories/MatiereRepository.java
+++ src/main/java/com/example/projet_ecole/repositories/MatiereRepository.java
@@ -1,7 +1,10 @@
 package com.example.projet_ecole.repositories;
 
+import com.example.projet_ecole.entities.Etudiant;
+import com.example.projet_ecole.entities.Matiere;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MatiereRepository {
+public interface MatiereRepository extends JpaRepository<Matiere, Integer> {
 }
